8101f9b5a7be28cc2db15b9b0c5f0837b00e5489
wilsoft-gt <wilson.romero1992@gmail.com>
2020-01-13 18:34:29 -0600

    added new server

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ function randomQuote() {
   	console.log()
     $('.quote').text('\"' + json.results[0].quote_quote + '\"');
     $('.quoteFooter').text(json.results[0].quote_author)
-    twitterShare.setAttribute("href", "https://twitter.com/share?url=&via=wilsoftGM&related=&text=" + encodeURIComponent(json.quote + "\n" +json.author)+ " -- ");
-    tumblrShare.setAttribute("href", "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes%2Cwilsoftbl&caption=" + encodeURIComponent(json.author)+"&content=" +encodeURIComponent(json.quote)+ "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button&_format=html");
+    twitterShare.setAttribute("href", "https://twitter.com/share?url=&via=wilsoftGM&related=&text=" + encodeURIComponent(json.results[0].quote_quote + "\n" +json.results[0].quote_author)+ " -- ");
+    tumblrShare.setAttribute("href", "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes%2Cwilsoftbl&caption=" + encodeURIComponent(json.results[0].quote_author)+"&content=" +encodeURIComponent(json.results[0].quote_quote)+ "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button&_format=html");
   });
 }
 
